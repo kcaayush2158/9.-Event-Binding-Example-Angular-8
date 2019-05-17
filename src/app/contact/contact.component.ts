41c3675e4a0d44f8646d762d2b121e290158c158
aayush kc <kcaayush2158@gmail.com>
2019-05-17 20:32:59 +0545

    Project uploaded style binding

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -4,41 +4,30 @@ import { removeDebugNodeFromIndex } from '@angular/core/src/debug/debug_node';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styles:['.text-success{color:green;}.text-danger{color:red;.text-special{font-style:italic}}'],
-  template: '<br><br><br><h2 [class]="sucessClass">Hello</h2><h2 [class.text-danger]="hasError">Code Evolution</h2> <br><h2 [ngClass]="messageClass">Message Class</h2>'
+  //template:'<h2>Welcome {{name}}</h2> <br> <h2 [style.color]="\'orange\'"> Style Binding</h2>',
+
+  // NO 2 -Conditional oprator to assign the value of css property
+  // template:' <h2 [style.color]="hasError ? \'orange\' : \'red\'"> Style Binding</h2>',
+  
+  //No 3- HighLight color using the varaible to style binding 
+  //template:' <h2 [style.color]="highLightColor"> Style Binding</h2>',
+
+  //No 4 - Using ngStyle directive to apply mutiple styles
+  template:' <h2 [ngStyle]="titleStyle"> Style Binding</h2>',
+  styles: []
 })
 
 
 export class ContactComponent implements OnInit {
-public sucessClass="text-success";
-public textDanger="text-danger";
-public textSpecial="text-special";
-public hasError = false;
+public name="Angular.js";
+public hasError = true;
 public isSpecial = true;
-public messageClass= {
-  "text-success" : !this.hasError, //true
-  "text-danger": this.hasError, //false
-  "text-special":this.isSpecial //true
+public highLightColor ="orange";
+public titleStyle = {
+  color:"blue",
+  fontStyle:"italic"
 }
-
   constructor() { }
-
   ngOnInit() {
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
